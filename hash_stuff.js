536498d6ff4a96969028d999a753e97063569fa1
u4ik <affirmativeamit@gmail.com>
2021-04-26 04:42:16 -0400

    hashing stuff....still a bit lost

--- hash_stuff.js
+++ hash_stuff.js
@@ -2,11 +2,42 @@ const person = {};
 person['firsName'] = 'bob';
 person['lastName'] = 'smith';
 
-// console.log(person['lastName']);
+console.log(person);
+
+const hashStringToInt = (s, tableSize) => {
+    let hash = 17;
+    for (let i = 0; i < s.length; i++) {
+        hash = (13 * hash * s.charCodeAt(i)) % tableSize
+    }
+
+    return hash;
+};
+
+class HashTable {
+
+    table = new Array(100);
+
+    setItem = (key, value) => {
+        const idx = hashStringToInt(key, this.table.length)
+        this.table[key] = value
+    }
+    getItem = key => {
+        const idx = hashStringToInt(key, this.table.length)
+        return this.table[key]
+    }
+}
 
 
+const myTable = new HashTable();
+myTable.setItem('firstName', 'Bob');
 
+myTable.setItem('lastName', 'Smith');
 
+myTable.setItem('age', 30);
 
+// console.log(myTable.getItem('firstName'));
+console.log(myTable.table);
+console.table(myTable.table);
 
+//WTF....am I doing....lol
 
--- parameter_spread_sort_arrays.js
+++ parameter_spread_sort_arrays.js
@@ -1,17 +1,17 @@
-let arr = [1000000000, 2121212, 30000, 4333, 500, 69, 47, 61];
+let arr =  [1000000000, 2121212, 30000, 4333, 500, 69, 47, 61];
 let arr2 = [4358, 21345, 3043, 45347, 345746, 4357, 7, 61];
 let arr3 = [1, undefined, true, NaN, null, 7, 8, 6, 3, 2, 3, 9, 5, 6];
-let arr4 = ['banana', 'watermelon', 'apricot', 'pudding', 'apple']
+let arr4 = ['banana', 'watermelon', 'apricot', 'pudding', 'apple'];
 
 class sortStuff {
     static someFuncToSortArrays = (...args) => {
         if (args.length > 0) {
             let sortArr = [];
-            args.forEach(item => {
-                if (typeof item.includes('string')) {
-                    sortArr.push(item.sort());
-                }else{
+            args.forEach((item,index) => {
+                if (typeof item[0] === 'number') {
                     sortArr.push(item.sort((a, b) => a - b));
+                }else if(typeof item[0] === 'string'){
+                    sortArr.push(item.sort());
                 };
             });
             return sortArr;
@@ -21,6 +21,7 @@ class sortStuff {
     };
 };
 
-let sortedArrays = sortStuff.someFuncToSortArrays(arr, arr2, arr3, arr4);
+let sortedArrays = sortStuff.someFuncToSortArrays(arr3, arr2, arr,arr4,['banana',1,2,'taco']);
 
 console.log(sortedArrays);
+
