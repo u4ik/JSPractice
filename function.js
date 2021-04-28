f973c9edd1af7bafc27c2a9a872963e81527a0f5
u4ik <affirmativeamit@gmail.com>
2021-04-28 07:34:20 -0400

    IIFE

--- function.js
+++ function.js
@@ -18,3 +18,10 @@ function square(x) {
 var y = 10;
 console.log(square(y));
 console.log(y);
+
+
+//IIFE - Immediately Invoked Function Expression
+//(...args) - Rest Parameter - allows infinite arguments, which then the values get stored into an array. args = [arg1,arg2,arg3,arg4]
+
+
+((...args) => console.log(args))(1,2,true,undefined,null,NaN,{color: 'green'})
