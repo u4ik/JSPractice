8ff908b6ac2e2ac3e52bc75987e78a1b5ae2e689
u4ik <affirmativeamit@gmail.com>
2021-04-28 07:28:06 -0400

    date fixes

--- date.js
+++ date.js
@@ -2,10 +2,11 @@ let date = new Date();
 let month = date.getMonth() + 1;
 let dayNum = date.getUTCDate();
 let year = date.getFullYear();
+let day = Date().substring(0,3);
 
 let fullDate = month+'/'+dayNum+'/'+year;
 
-console.log(fullDate);
+// console.log(fullDate);
 
 var hours = date.getHours();
 var minutes = date.getMinutes();
@@ -15,7 +16,8 @@ hours = hours ? hours : 12;
 minutes = minutes < 10 ? '0' + minutes : minutes;
 var stringTime = hours + ':' + minutes + ' ' + ampm;
 
-console.log(stringTime, fullDate);
+console.log(day, fullDate, stringTime);
+
 
 
 
