064afa26921f8855bc61768ae408d958ccc069f4
u4ik <affirmativeamit@gmail.com>
2021-05-02 22:35:43 -0400

    more notes + advanced examples

--- async_promises.js
+++ async_promises.js
@@ -1,6 +1,7 @@
 const fetch = require('node-fetch');
 
-const newFunc = (flag) =>  {
+
+const newFunc = (flag) => {
     // await results();
     console.log('Success in getting Data!', flag);
 };
@@ -32,8 +33,8 @@ const results2 = async () => {
 const displayName = (data, flag) => {
     console.log(data.name, flag)
 };
-results();
-results2();
+// results();
+// results2();
 
 
 // Promise.all
@@ -44,12 +45,12 @@ let rmFetch = fetch('https://rickandmortyapi.com/api/character');
 
 const promiseAllFetch = () => {
     Promise.all([swFetch, rmFetch])
-    .then(values => {
-        return Promise.all(values.map(r => r.json()));
-    }).then(([sw, rm]) => {
-        console.log(sw.name);
-        console.log(rm.results[0].name);
-    }).catch(err => console.error(err));  
+        .then(values => {
+            return Promise.all(values.map(r => r.json()));
+        }).then(([sw, rm]) => {
+            console.log(sw.name);
+            console.log(rm.results[0].name);
+        }).catch(err => console.error(err));
 };
 
 // promiseAllFetch();
@@ -77,3 +78,116 @@ function fetchAll() {
 };
 
 // fetchAll();
+
+
+const tick = Date.now();
+
+const log = v => console.log(`${v} \n Elapsed: ${Date.now() - tick}ms`);
+
+const codeBlocker = () => {
+    //! Slow!
+    // let i = 0;
+    // while (i < 1000000000) { i++ };
+    // return 'Loops Done!'
+
+    //! Close, but no cigar, still slow!
+    // return new Promise((resolve, reject) => {
+    //     let i = 0;
+    //     while (i < 1000000000) { i++ };
+    //     resolve('Loops Done!')
+    // })
+
+    //! Speedy boi!!!!
+    return Promise.resolve().then(v => {
+        let i = 0;
+        while (i < 1000000000) { i++ }
+        return '??????';
+    });
+};
+
+// log('Synchronous 1');
+// log(codeBlocker());
+// log('Synchronous 2');
+
+const getFruit1 = async (name) => {
+    const fruits = {
+        pineapple: '🍍',
+        peach: '🍑',
+        strawberry: '🍓'
+    };
+    //Having async will pretty much wrap the return in a promise resolve
+    return fruits[name]
+};
+
+
+// console.log(getFruit1('strawberry'));
+
+
+//? OR
+
+const getFruit = (name) => {
+    const fruits = {
+        pineapple: '🍍',
+        peach: '🍑',
+        strawberry: '🍓'
+    };
+    return Promise.resolve(fruits[name])
+};
+
+// getFruit('peach').then(console.log);
+
+const makeSmoothie = async () => {
+    const a = await getFruit('pineapple');
+    const b = await getFruit1('strawberry');
+
+    return [a, b];
+};
+
+// makeSmoothie().then(console.log)
+
+//? VS: 
+
+const makeSmoothie2 = () => {
+    let holder;
+    return getFruit('strawberry')
+        .then(fruit1 => {
+            holder = fruit1
+            return getFruit('pineapple')
+        })
+        .then(fruit2 => [fruit2, holder])
+};
+
+// makeSmoothie2().then(console.log)
+
+//? Promise.all async
+
+const makeSmoothie3 = async () => {
+    const a = getFruit('pineapple');
+    const b = getFruit1('strawberry');
+    const smoothie = await Promise.all([a, b])
+    return smoothie;
+};
+
+// makeSmoothie3().then(console.log)
+
+//? Loop promise
+
+const fruits = ['peach', 'pineapple', 'strawberry'];
+const smoothie = fruits.map(fruit => getFruit(fruit));
+
+const fruitLoop = async () => {
+    for await (const emoji of smoothie) {
+        log(emoji)
+    }
+};
+
+// fruitLoop();
+
+
+const fruitInspection = async () => {
+    if (await getFruit('peach') === '🍑') {
+        console.log('Looks peachy!');
+    };
+};
+
+fruitInspection();
