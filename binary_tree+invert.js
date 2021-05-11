5e98e1cfc59d59981ec4442c8cd2328b0ddebd29
u4ik <affirmativeamit@gmail.com>
2021-05-11 17:34:55 -0400

    binary search

--- binary_tree+invert.js
+++ binary_tree+invert.js
@@ -45,7 +45,7 @@ let node = {
 };
 
 const reverseNodes = (node) => {
-    if(node == 'undefined'){
+    if (node == 'undefined') {
         node = null;
     }
     if (node == null) {
@@ -62,7 +62,7 @@ const reverseNodes = (node) => {
 reverseNodes(node);
 
 
-console.log(treeify(node));
+// console.log(treeify(node));
 
 /* Great, it's inverted....but I shouldn't invert it in this way....the left rights are included in the children too...
  ............scalability!! Lol...yea idk.
@@ -91,5 +91,39 @@ console.log(treeify(node));
 */
 
 
-let array = [1,2,3,4,5,6,7,8,9,10];
+let array = [13, 16, 19, 25, 28, 34, 39, 42, 53, 55, 62, 67, 68, 70, 75, 82, 99, 103, 104, 105, 107, 107, 108, 109, 110, 111, 112, 115, 119, 120, 150, 180, 200, 201, 202, 203, 204, 205, 207, 210, 220, 250, 270, 290];
 
+const findSomething = (target) => {
+    for (let i = 0; i < array.length; i++) {
+        if (array[i] == target) {
+            return array[i];
+        };
+    };
+};
+
+let tempArr = [1,2,3,4,5,6]
+
+console.time('Iterative Search NON BINARY');
+console.log(findSomething(201));
+console.timeEnd('Iterative Search NON BINARY');
+
+const findSomethingBinaryWay = (target) => {
+    let left = 0;
+    let right = array.length - 1;
+
+    while (left < right) {
+        let mid = Math.floor((left + right) / 2);
+        if (target === array[mid]) {
+            return array[mid];
+        } else if (target < array[mid]) {
+            right = mid - 1;
+        } else {
+            left = mid + 1;
+        };
+    };
+    return 'Not Found';
+};
+
+console.time('Iterative Search BINARY');
+console.log(findSomethingBinaryWay(220));
+console.timeEnd('Iterative Search BINARY');
