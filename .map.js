8bf4202b7497f0aadeed25bda2535867050ac9c4
u4ik <affirmativeamit@gmail.com>
2021-05-11 11:23:32 -0400

    added more to map. GC functional

--- .map.js
+++ .map.js
@@ -2,16 +2,28 @@ let arr = [1, 2, 3, 4, 5, 6, 7, 8, { name: 'Joe' }, true, 'something', obj = { a
 
 
 arr.map((item, index) => {
-    console.log(item.name);
+    if(item.name !== undefined){
+        console.log(item.name);
+    }
 });
 
 
 let arr1 = [{ user: 'taco', age: 50 }, { user: 'banana', age: 70 }, { user: 'taco', age: 50 }];
-let obj = arr1.map((info) => ({
+let obj1 = arr1.map((info) => ({
 
     username: info.user,
     age: info.age
 
 }));
 
-console.log(obj);
+console.log(obj1);
+
+let dogArray = ['lab', 'shiba inu', 'husky', 'st bernard', 'golden retriever'];
+
+const dogBark = (dog) => {
+    return `The ${dog} went woof!`;
+};
+
+dogArray.map((doggy) => {
+    console.log(dogBark(doggy));
+});
--- garbage_collection_nullify.js
+++ garbage_collection_nullify.js
@@ -1,25 +1,26 @@
 //Reference count: When ref count reaches 0, the garbage collector will go into effect and clear up current memory that's allocated.
 //What if we just nullify or 'undefine' the items we wish to forcefully reach a reference count of 0??
-(function(){
-    var x = 5;
-    let obj1 = {};
-    let obj2 = {};
-    let obj3 = {};
-    let obj4 = {};
-    let obj5 = {};
-})();
+var x = 5;
+var obj1 = {};
+let obj2 = {};
+let obj3 = {};
+let obj4 = {};
+let obj5 = {};
+
+console.log(x,obj1); 
 
 
-const nullify = (x) => {
+const nullify = () => {
     x = null;
+    obj1 = null;
 };
 
-nullify(x);
+nullify();
 
 // setTimeout(() => {
 //     console.log(x); 
 // },500)
 
-console.log(x); 
+console.log(x,obj1); 
 
 
