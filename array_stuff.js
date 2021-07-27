1638abc3316fc06b792f97629fb9e856a536a0bf
u4ik <affirmativeamit@gmail.com>
2021-07-27 04:07:41 -0400

    added dupe array

--- array_stuff.js
+++ array_stuff.js
@@ -1,16 +1,16 @@
 
 //Arr Numbers Get Random
 let arr1 = [1, 2, 3, 4, 5, 6, 7];
-let arr1 = ['firstItem', 'secondItem', 'thirdItem', 'fourthItem'];
+arr1 = ['firstItem', 'secondItem', 'thirdItem', 'fourthItem'];
 
 //Arr Pets Get Random
-let arr1 = ['cat', 'dog', 'bird', 'turtle', 'fish', 'raccoon', 'snake'];
-console.log(arr1[Math.floor(Math.random() * arr1.length)]);
+arr1 = ['cat', 'dog', 'bird', 'turtle', 'fish', 'raccoon', 'snake'];
+// console.log(arr1[Math.floor(Math.random() * arr1.length)]);
 
 
 //Mutable Remove Crap From An Array/ Loop Starts At The End
 
-let arr = [{ key: 'value' }, 'cat', 5, undefined,null,"hello", "lalala", 45, 'dog', 19, '7'];
+let arr = [{ key: 'value' }, 'cat', 5, undefined, null, "hello", "lalala", 45, 'dog', 19, '7'];
 
 for (let i = arr.length - 1; i >= 0; i--) {
     if (typeof arr[i] !== 'number') {
@@ -18,29 +18,27 @@ for (let i = arr.length - 1; i >= 0; i--) {
     };
 };
 
-console.log(arr); // [ 5, 45, 19 ]
+// console.log(arr); // [ 5, 45, 19 ]
 
 
 //Find duplicates in an array
 
-let arr1 = [1, 2, 3, 4, 5, 6]
-let arr2 = [1, 2, 2, 5, 7, 8]
-let arr3 = [1, 5, 2, 5, 7, 8,7,1]
+let numArr1 = [1, 2, 3, 4, 5, 6]
+let numArr2 = [1, 2, 2, 5, 7, 8]
+let numArr3 = [1, 5, 2, 5, 7, 8, 7, 1]
 
 function checkArr(arr) {
-	const arraywithoutdupes = [...new Set(arr)];
+    const arraywithoutdupes = [...new Set(arr)];
 
-	let duplicates = [...arr];
-	
-	arraywithoutdupes.forEach((item) => {
-		const i = duplicates.indexOf(item)
-		duplicates = duplicates.slice(0,i)
-		.concat(duplicates.slice(i+1,duplicates.length))
-		// console.log(duplicates);
-	})
-	console.log(duplicates)
-	
+    let duplicates = [...arr];
+
+    arraywithoutdupes.forEach((item) => {
+        const i = duplicates.indexOf(item)
+        duplicates = [...duplicates.slice(0, i), ...duplicates.slice(i + 1, duplicates.length)]
+    })
+    console.log({duplicates})
+
 }
 
-checkArr(arr3)
+checkArr(numArr3)
 
