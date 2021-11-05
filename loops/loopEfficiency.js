1f7b3c6e360dd953c69767922b28e2e326cadca5
u4ik <insighteuphoric@gmail.com>
2021-11-05 02:19:56 -0400

    fixed loop efficiency, tradional for loops are the fastest!

--- loops/loopEfficiency.js
+++ loops/loopEfficiency.js
@@ -1,29 +1,39 @@
-let steps = 40000000
-let stepsArr = new Array(steps)
-
+let steps = 4000000
+let stepsArr = [...Array(steps).keys()]
 
 console.time('For of Loop:')
-for (step of new Array(steps)) {
-    if (step === steps.length) {
-        console.log(step);
+for (step of stepsArr) {
+    if (step === stepsArr.length-1) {
+        console.log('For of loop - Finished');
     }
 }
 console.timeEnd('For of Loop:')
 
 console.time('Traditional Loop:')
-for (i = 0; i < steps; i++) {
-    if (i === steps) {
-        console.log('Finished');
+for (i = 0; i < stepsArr.length; i++) {
+    if (i === stepsArr.length-1) {
+        console.log('Traditional for loop - Finished');
     }
 }
 console.timeEnd('Traditional Loop:')
 
 console.time('For in Loop:')
-for (step in new Array(steps)) {
-    if (step === steps.length - 1) {
-        console.log('Finished');
+for (step in stepsArr) {
+    if (+step === stepsArr.length-1) {
+        console.log('For in loop - Finished');
     }
 }
 console.timeEnd('For in Loop:')
 
-//* Need to fix some things
+/*
+        OUTPUT
+    Fastest to slowest
+__________________________
+Traditional for loop - Finished
+? Traditional Loop:: 11.237ms
+For of loop - Finished
+? For of Loop:: 47.233ms
+For in loop - Finished
+? For in Loop:: 840.516ms
+
+*/
